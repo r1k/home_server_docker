b741bf1699f41882711ee9ba229194a6f1b3ee64
Richard Kelsey <3041753+r1k@users.noreply.github.com>
2024-02-25 20:41:59 +0000

    update create_networks for docker instance

--- create_networks.sh
+++ create_networks.sh
@@ -5,13 +5,13 @@ if [ ! $(docker network ls -q -f name=reverse-proxy) ]; then
 fi
 
 if [ ! $(docker network ls -q -f name=macvlan_net) ]; then
-  sudo ip link set dev enp37s0 promisc on
+  sudo ip link set dev eth0 promisc on
   # create docker network 
   # 192.168.221.16 - 192.168.221.31 where 192.168.221.31 will be the host bridge
   docker network create --driver macvlan --subnet 192.168.221.0/24 --gateway 192.168.221.254 --ip-range 192.168.221.16/28 --aux-address 'host=192.168.221.31' -o parent=enp37s0 macvlan_net
 
   # now setup the host bridge to be able to reach the addresses on the above network
-  sudo ip link add macvlan_shim link enp37s0 type macvlan mode bridge
+  sudo ip link add macvlan_shim link eth0 type macvlan mode bridge
   # give the host an address on this interface
   sudo ip addr add 192.168.221.31/32 dev macvlan_shim
   # bring up the link
