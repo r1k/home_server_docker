9c4b72ad07407860dba3188eabd09f841a3ef33c
r1k <3041753+r1k@users.noreply.github.com>
2025-03-21 17:17:56 +0000

    Update LiteLLM setup script to check for existing directory and modify docker-compose.yml path

--- LiteLLM/setup.sh
+++ LiteLLM/setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-git clone https://github.com/BerriAI/litellm.git
+if [[ ! -d "litellm" ]]; then
+    git clone https://github.com/BerriAI/litellm.git
+fi
 
+rm litellm/.env || true
 # create env file
 RANDOM_KEY=$(head /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 16)
 echo "LITELLM_MASTER_KEY=sk-$RANDOM_KEY" >> litellm/.env
 
 RANDOM_KEY=$(head /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 16)
 echo "LITELLM_SALT_KEY=sk-$RANDOM_KEY" >> litellm/.env
+
+# store data on the file system
+sed -i -E 's/postgres_data:\/var\/lib\/postgresql\/data/\$DOCKER_PERSIST\/LiteLLM:\/var\/lib\/postgresql\/data/gm;t' litellm/docker-compose.yml
