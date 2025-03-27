7fe7b6bd95f93251f604aea45d1432c5a74034bc
r1k <3041753+r1k@users.noreply.github.com>
2025-03-27 13:15:31 +0000

    LiteLLM

--- LiteLLM/setup.sh
+++ LiteLLM/setup.sh
@@ -21,4 +21,7 @@ RANDOM_KEY=$(head /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 16)
 echo "LITELLM_SALT_KEY=sk-$RANDOM_KEY" >> litellm/.env
 
 # store data on the file system
-sed -i -E 's/postgres_data:\/var\/lib\/postgresql\/data/\$DOCKER_PERSIST\/LiteLLM:\/var\/lib\/postgresql\/data/gm;t' litellm/docker-compose.yml
+sed -i -E 's/postgres_data:\/var\/lib\/postgresql\/data/\$DOCKER_PERSIST\/LiteLLM\/postgres:\/var\/lib\/postgresql\/data/gm;t' litellm/docker-compose.yml
+
+sed -i -E '/#########/a \    volumes:\n       - \$DOCKER_PERSIST\/LiteLLM\/config.yaml:\/app\/config.yaml' litellm/docker-compose.yml
+
